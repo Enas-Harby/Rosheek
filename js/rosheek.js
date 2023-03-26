9a52cbc5c992db29309921f2c0b3198b85e51f64
Enas-Harby <120248979+Enas-Harby@users.noreply.github.com>
2023-03-26 11:37:01 +0200

    Add files via upload

---
+++ js/rosheek.js
@@ -0,0 +1,9 @@
+document.getElementById("one").onfocus = function () {
+    location.href = "product.html";
+}
+
+// document.getElementById("one").addEventListener("click", myFunction);
+
+// function myFunction() {
+//     document.getElementById("one").innerHTML = "YOU CLICKED ME!";
+//   }
